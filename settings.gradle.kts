edc33e79b908d389766aed5b8cc52a69602cace4
germanafro <andreasberger86@hotmail.de>
2023-02-13 14:27:47 +0100

    add spring boot starter project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "dw2-mongodb-importer-exporter-tool"
---
+++ src/main/kotlin/org/inc/dw2mongodbimporterexportertool/Dw2MongodbImporterExporterToolApplication.kt
@@ -0,0 +1,11 @@
+package org.inc.dw2mongodbimporterexportertool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Dw2MongodbImporterExporterToolApplication
+
+fun main(args: Array<String>) {
+	runApplication<Dw2MongodbImporterExporterToolApplication>(*args)
+}
